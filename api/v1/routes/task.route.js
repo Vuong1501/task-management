0bb2defe532e13895fa9d3e7831df782f1f28ce0
Vuong1501 <vuong.44.10a5@gmail.com>
2024-08-06 16:26:02 +0700

    Bộ lọc theo trạng thái

--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -1,31 +1,12 @@
 const express = require('express');
 const router = express.Router(); 
 
-const Task = require("../../../models/task.model");
+const controller = require("../../../api/v1/controllers/task.controller");
 
 
-router.get("/", async (req, res) => {
+router.get("/", controller.index);
 
-    const tasks = await Task.find({
-        deleted: false
-    });
-
-    res.json(tasks);
-});
-
-router.get("/detail/:id", async (req, res) => {
-
-    try {
-        const id = req.params.id;
-        const task = await Task.findOne({
-            _id: id,
-            deleted: false
-        });
-        res.json(task);
-    } catch (error) {
-        res.json("Không tìm thấy");
-    } 
-});
+router.get("/detail/:id", controller.detail);
 
 
 module.exports = router; 
