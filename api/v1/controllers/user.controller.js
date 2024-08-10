a11b8d366ed012300186466bc2eb0c5ee716426a
Vuong1501 <vuong.44.10a5@gmail.com>
2024-08-10 20:49:19 +0700

    Màn hình reset mật khẩu

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -152,3 +152,36 @@ module.exports.otpPassword = async (req, res) => {
     });
     
 };
+
+//[POST]/api/v1/users/password/reset
+module.exports.resetPassword = async (req, res) => {
+   
+    const token = req.body.token;
+    const password = req.body.password;
+
+    const user = await User.findOne({
+        token: token
+    });
+
+    if(md5(password) === user.password){
+        res.json({
+            code: 400,
+            massage: "Mật khẩu mới phải khác mật khẩu cũ",
+        });
+        return;
+    }
+    
+    //Cập nhật lại mật khẩu
+    await User.updateOne({
+        token: token
+    }, {
+        password: md5(password)
+    });
+
+
+    res.json({
+        code: 200,
+        massage: "Đổi mật khẩu thành công!",
+    });
+    
+};
--- api/v1/routes/user.route.js
+++ api/v1/routes/user.route.js
@@ -12,6 +12,8 @@ router.post("/password/forgot", controller.forgotPassword);
 
 router.post("/password/otp", controller.otpPassword);
 
+router.post("/password/reset", controller.resetPassword);
+
 
 
 module.exports = router; 
